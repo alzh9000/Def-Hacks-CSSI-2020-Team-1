30525b472bb62bd877d9f7efb507e709466675bf
alzh9000 <32472855+alzh9000@users.noreply.github.com>
2020-06-20 17:28:55 -0400

    added the Animate on Scroll library to each page

--- js/form.js
+++ js/form.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     AOS.init();
 });
+
+function getRecipes() {
+    return true;
+}
+
+fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
