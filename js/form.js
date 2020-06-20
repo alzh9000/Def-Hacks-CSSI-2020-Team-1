1156193c2178a9e2b396912fb70f054265b93f2d
kyang8093 <kgyang@stanford.edu>
2020-06-20 17:58:10 -0600

    form updates
    Co-authored-by: yuyuan-luo <luo.yuyuan@yahoo.com>

--- js/form.js
+++ js/form.js
@@ -6,12 +6,18 @@ $(document).ready(function () {
         for (var i = 0; i < obj.length; i++) {
             let image = obj[i].imagelink;
             let name = obj[i].name;
-            var newCard = '<div class="card col-12"> <img class="card-img-top" src="' + image + '" alt="Equipment Image"> <div class="card-body"> <h5 class="card-title">' + name + '</h5> <div class="custom-control custom-checkbox"> <input type="checkbox" class="custom-control-input" id="' + name + '"> <label class="custom-control-label" for="' + name + '">Got it!</label> </div> </div> </div>';
+            var newCard = '<div id="' + name + '" data-aos="zoom-in" class="card col-12"> <img class="card-img-top" src="' + image + '" alt="Equipment Image"> <div class="card-body"> <h5 class="card-title">' + name + '</h5> <div class="custom-control custom-checkbox"> <input type="checkbox" class="custom-control-input" id="' + i + '"> <label class="custom-control-label" for="' + i + '">Got it!</label> </div> </div> </div>';
             $(".card-columns").append(newCard);
         }
     });
 
-
+    $('.searchbox-input').on('keyup', function () {
+        console.log(filter);
+        $('.card').show();
+        var filter = $(this).val(); // get the value of the input, which we filter on
+        console.log(filter);
+        $('#eq-cards').find(".card-title:not(:contains(" + filter + "))").parent().css('display', 'none');
+    });
 
 });
 
