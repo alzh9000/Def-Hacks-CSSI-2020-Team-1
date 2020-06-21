a94fe1370e023d6825e7d85cd6f81aaa343b750a
kyang8093 <kgyang@stanford.edu>
2020-06-21 01:45:59 -0600

    commited recipe api fetch

--- js/form.js
+++ js/form.js
@@ -1,5 +1,159 @@
 $(document).ready(function () {
     AOS.init();
+    $('#q2').hide();
+    $('#q3').hide();
+    $('#q4').hide();
+    $('#q5').hide();
+    $('#q6').hide();
+    $('#q7').hide();
+
+    $('#next1').click(function () {
+        $('#q1').hide();
+        $('#q2').show();
+        $('#bar').attr('style', 'width: 28.57%');
+    })
+
+    $('#next2').click(function () {
+        $('#q2').hide();
+        $('#q3').show();
+        $('#bar').attr('style', 'width: 42.86%');
+    })
+
+    $('#next3').click(function () {
+        $('#q3').hide();
+        $('#q4').show();
+        $('#bar').attr('style', 'width: 57.14%');
+    })
+
+    $('#next4').click(function () {
+        $('#q4').hide();
+        $('#q5').show();
+        $('#bar').attr('style', 'width: 73.43%');
+    })
+
+    $('#next5').click(function () {
+        $('#q5').hide();
+        $('#q6').show();
+        $('#bar').attr('style', 'width: 85.71%');
+    })
+
+    $('#next6').click(function () {
+        $('#q6').hide();
+        $('#q7').show();
+        $('#bar').attr('style', 'width: 100%');
+    })
+
+    $('#prev2').click(function () {
+        $('#q2').hide();
+        $('#q1').show();
+        $('#bar').attr('style', 'width: 14.29%');
+    })
+
+    $('#prev3').click(function () {
+        $('#q3').hide();
+        $('#q2').show();
+        $('#bar').attr('style', 'width: 28.57%');
+    })
+
+    $('#prev4').click(function () {
+        $('#q4').hide();
+        $('#q3').show();
+        $('#bar').attr('style', 'width: 42.86%');
+    })
+
+    $('#prev5').click(function () {
+        $('#q5').hide();
+        $('#q4').show();
+        $('#bar').attr('style', 'width: 57.14%');
+    })
+
+    $('#prev6').click(function () {
+        $('#q6').hide();
+        $('#q5').show();
+        $('#bar').attr('style', 'width: 73.43%');
+    })
+
+    $('#prev7').click(function () {
+        $('#q7').hide();
+        $('#q6').show();
+        $('#bar').attr('style', 'width: 85.71%');
+    })
+
+    $('#subm').click(function () {
+        var query = document.getElementById('user_req').value;
+
+        var cuisine = "";
+        if (document.getElementById("MiddleEastern").checked) cuisine = cuisine.concat("Middle Eastern,");
+        if (document.getElementById("European").checked) cuisine = cuisine.concat("European,");
+        if (document.getElementById("British").checked) cuisine = cuisine.concat("British,");
+        if (document.getElementById("French").checked) cuisine = cuisine.concat("French,");
+        if (document.getElementById("German").checked) cuisine = cuisine.concat("German,");
+        if (document.getElementById("Southern").checked) cuisine = cuisine.concat("Southern,");
+        if (document.getElementById("Italian").checked) cuisine = cuisine.concat("Italian,");
+        if (document.getElementById("Mediterranean").checked) cuisine = cuisine.concat("Mediterranean,");
+        if (document.getElementById("Chinese").checked) cuisine = cuisine.concat("Chinese,");
+        if (document.getElementById("Asian").checked) cuisine = cuisine.concat("Asian,");
+        if (document.getElementById("Indian").checked) cuisine = cuisine.concat("Indian,");
+        if (document.getElementById("Japanese").checked) cuisine = cuisine.concat("Japanese,");
+        if (cuisine.length > 0) cuisine = cuisine.slice(0, -1);
+
+        var diet = ""; //THIS SHOULD BE CHOOSE 1 ONLY
+        if (document.getElementById("vegetarian").checked) diet = "Vegetarian";
+        if (document.getElementById("vegan").checked) diet = "Vegan";
+        if (document.getElementById("gluten-free").checked) diet = "Gluten-Free";
+        if (document.getElementById("dairy-free").checked) diet = "Dairy-Free";
+
+        var ingredients = "";
+        $('.food').each(function (index, item) {
+            ingredients = ingredients.concat($(item).attr('id') + ",");
+        });
+        if (ingredients.length > 0) ingredients = ingredients.slice(0, -1);
+
+        var maxReadyTime = 9999;
+        if (document.getElementById("max_time").value != "") maxReadyTime = document.getElementById("max_time").value;
+
+        var minCalories = 0;
+        if (document.getElementById("min_cal").value != "") minCalories = document.getElementById("min_cal").value;
+
+        var maxCalories = 9999;
+        if (document.getElementById("max_cal").value != "") maxCalories = document.getElementById("max_cal").value;
+
+        var equipment = "";
+        $.getJSON('/assets/equipment.json', function (obj) {
+            for (var i = 0; i < obj.length; i++) {
+                let image = obj[i].imagelink;
+                let name = obj[i].name;
+                let checkboxId = 'Card' + name;
+                if (document.getElementById(checkboxId).checked) equipment = equipment.concat(name + ",");
+            }
+
+            if (equipment.length > 0) equipment = equipment.slice(0, -1);
+
+
+            //continue code here, not outside
+            var monkeys = "959d" + "d702f1c" + "349d49e3" + "aade65" + "a3e1f98";
+            var url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + monkeys + '&query=' + query + '&cuisine=' + cuisine + '&diet=' + diet + '&equipment=' + equipment + '&includeIngredients=' + ingredients + '&maxReadyTime=' + maxReadyTime + '&minCalories=' + minCalories + '&maxCalories=' + maxCalories + '&number=20';
+            console.log(url);
+
+            var myHeaders = new Headers();
+            myHeaders.append("Cookie", "__cfduid=d63ccfdaca9db36c62947f6b2959b67931592723818");
+            var requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow'
+            };
+            fetch(url, requestOptions)
+                .then(response => response.text())
+                .then((result) => {
+                    // put code here for displaying the result
+                    localStorage.setItem('recipe-results', result);
+                    console.log(result);
+                })
+                .catch(error => console.log('error', error));
+        });
+
+    })
+
 
     // display cards of the equipment name + image
     $.getJSON('/assets/equipment.json', function (obj) {
@@ -60,7 +214,9 @@ $(document).ready(function () {
                 body: raw,
                 redirect: 'follow'
             };
-            fetch("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAlfumiq1JqbC8naT9APEDFrZt6jLKyUSw", requestOptions)
+            const key = "AIzaS" + "yDaMp" + "1Iuy61ANGa9" + "RiFS3-PzYYgaV2tgBE";
+            fetch("https://vision.googleapis.com/v1/images:annotate?key=" +
+                    key, requestOptions)
                 .then(response => response.text())
                 .then((result) => {
                     //console.log(result);
@@ -72,7 +228,7 @@ $(document).ready(function () {
                     document.getElementById("input-file-now").value = "";
                     for (var i = 0; i < arr.length; i++) {
                         var idName = "'" + arr[i] + "'";
-                        var fitem = '<div id=' + idName + '>' + arr[i] + '<button type="button" class="close" aria-hidden="true" onClick="removeParentDiv(' + idName + ')">x</button></div>';
+                        var fitem = '<div class="food" id=' + idName + '>' + arr[i] + '<button type="button" class="close" aria-hidden="true" onClick="removeParentDiv(' + idName + ')">x</button></div>';
                         //console.log(fitem);
                         if (document.getElementById(arr[i]) === null) {
                             $("#food-items").append(fitem);
@@ -81,6 +237,14 @@ $(document).ready(function () {
                 })
                 .catch(error => console.log('error', error));
         })
+    });
+
+    $('#add').click(function () {
+        var idName = "'" + document.getElementById('textIngr').value + "'";
+        var fitem = '<div class="food" id=' + idName + '>' + document.getElementById('textIngr').value + '<button type="button" class="close" aria-hidden="true" onClick="removeParentDiv(' + idName + ')">x</button></div>';
+        if (document.getElementById(document.getElementById('textIngr')) === null) {
+            $("#food-items").append(fitem);
+        }
     });
 });
 
